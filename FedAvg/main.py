1ef162197aa1df0aaeb52f57fe8fedeb5e683e61
Anubhav3084 <anubhav84prasad@gmail.com>
2023-02-25 12:27:20 +0530

    Update main.py

--- FedAvg/main.py
+++ FedAvg/main.py
@@ -4,6 +4,7 @@ from FedAvg.utils import *
 from FedAvg.models import *
 from FedAvg.train import *
 from torchvision import datasets, transforms
+import random
 
 transforms_mnist = transforms.Compose([
     transforms.ToTensor(),
@@ -22,7 +23,19 @@ iid_dict = iid_partition(mnist_data_train, 100)
 
 model = LeNet()
 
+# set manual seed for reproducibility
+seed = 42
+
+# general reproducibility
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+
 if torch.cuda.is_available():
     model.cuda()
+
+# gpu training specific
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 mnist_cnn_iid_trained = training(model, rounds, batch_size, lr, mnist_data_train, iid_dict, C, K, E)
